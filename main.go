1b16064cf51788c2bab4f0733877875a8983b793
Miki5535 <masterfin787@gmail.com>
2025-03-07 14:07:21 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 	"go-grom/controller"
+	"net/http"
 
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-func main() {
-	// รูปแบบการใส่
-	// dsn := "user:pwd@tcp(ip:port)/dbname?collation=utf8mb4_unicode_ci&parseTime=true"
+// ฟังก์ชันที่ใช้ในการจัดการคำขอ HTTP
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!") // ส่งข้อความตอบกลับไปยังผู้ใช้
+}
 
-	// ใช้ไฟล์ config.yaml เก็บค่า config ต่างๆ เช่นค่า dsn
-	// dsn := "landmark:landmark@csmsu@tcp(202.28.34.197:3306)/landmark?collation=utf8mb4_unicode_ci&parseTime=true"
+func main() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
@@ -27,74 +28,20 @@ func main() {
 
 	dialactor := mysql.Open(dsn)
 
-	// _ แปลว่า มีตัวแปลอยู่นะแต่ยังไม่ให้เอาไปเช็ค หรือ ใช้ เพื่อไม่ให้ err
-	// db, err := gorm.Open(dialactor)
 	_, err = gorm.Open(dialactor)
-	// db,err := gorm.Open(dialactor)
 	if err != nil {
 		panic(err)
-
 	}
 
-	// StartServer web API
+	// กำหนด HTTP route ที่เรียกใช้ handler
+	http.HandleFunc("/", handler) // เมื่อเข้าถึง URL "/" จะเรียก handler
 
-	controller.StartServer()
-	// print("connnn ssssss")
+	// เริ่ม HTTP server
+	go controller.StartServer() // หากคุณต้องการรัน server ของ controller ใน background
 
-	// countries := []model.Country{}
-	// db.Find(&countries)
-
-	// // fmt.Printf("%v", countries)
-
-	// // gpt
-	// landmarks := []model.Landmark{}
-	// // db.Select("name").Find(&landmarks)
-	// db.Find(&landmarks)
-	// // for _, landmark := range landmarks {
-	// // 	fmt.Println(landmark.Name)
-	// // }
-
-	// // fmt.Printf("%v", landmarks)
-
-	// // Preload ใช้เรียก sql แบบเล็กๆ หลายๆ ก้อน ส่วน Joins เรียก ข้อมูล ใหญ่ๆมาครั้งเดียวเลย
-	// // Joins ใช้ query น้อยกว่า  ซับซ้อนกว่า มักใช้การซ้อนข้อมูลเยอะๆ  โดยทั่วๆไปดูเหมือนจะเร็วกว่า
-	// // db.Preload("CountryData").Find(&landmarks)
-	// // db.Joins("CountryData").Find(&landmarks)
-
-	// db.Where("Name like ? or Name = ?", "%ไทย%", "อิตาลี").Find(&countries)
-	// // fmt.Printf("%v", countries)
-
-	// country := model.Country{}
-	// db.Where("Name like ? or Name = ?", "%ไทย%", "อิตาลี").First(&country)
-	// // fmt.Printf("%v", country)
-
-	// db.Limit(3).Order("Name").Find(&countries)
-	// db.Limit(3).Order("Name desc").Find(&countries)
-	// fmt.Printf("%v", countries)
-
-	// // Distinct  แปลว่าเอามาแค่ที่ ไม่ซ้ำกัน
-	// db.Distinct("Idx").Find(&countries)
-	// fmt.Println(countries)
-
-	// type unique struct {
-	// 	Idx int
-	// }
-
-	// uniques := []unique{}
-	// db.Model(model.Country{}).Distinct("Idx").Find(&uniques)
-	// fmt.Println(uniques)
-
-	// country := model.Country{Name: "USA"}
-	// result := db.Create(&country)
-	// if result.Error != nil {
-	// 	panic(result.Error)
-	// }
-	// if result.RowsAffected > 0 {
-	// 	println("sssssssssssss")
-	// }
-
-	// countries := []model.Country{}
-	// db.Find(&countries)
-	// fmt.Printf("%v", countries)
-
+	// เริ่ม HTTP server
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
