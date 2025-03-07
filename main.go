7f98e28656135380bb120cce672c883a27a6169a
Miki5535 <masterfin787@gmail.com>
2025-03-07 14:10:32 +0700

    Update main.go2

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-	"go-grom/controller"
 	"net/http"
-
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// ฟังก์ชันที่ใช้ในการจัดการคำขอ HTTP
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!") // ส่งข้อความตอบกลับไปยังผู้ใช้
+// ฟังก์ชันที่เป็น exported ซึ่งจะถูกใช้โดย Vercel
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
+	// การอ่านไฟล์ config ด้วย viper
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
@@ -26,18 +25,15 @@ func main() {
 	fmt.Println(viper.Get("mysql.dsn"))
 	dsn := viper.GetString("mysql.dsn")
 
+	// การเชื่อมต่อฐานข้อมูล MySQL
 	dialactor := mysql.Open(dsn)
-
 	_, err = gorm.Open(dialactor)
 	if err != nil {
 		panic(err)
 	}
 
-	// กำหนด HTTP route ที่เรียกใช้ handler
-	http.HandleFunc("/", handler) // เมื่อเข้าถึง URL "/" จะเรียก handler
-
-	// เริ่ม HTTP server
-	go controller.StartServer() // หากคุณต้องการรัน server ของ controller ใน background
+	// กำหนด HTTP route ที่ใช้ handler
+	http.HandleFunc("/", Handler)
 
 	// เริ่ม HTTP server
 	fmt.Println("Starting server on :8080")
